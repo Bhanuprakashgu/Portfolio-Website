a6ae32fdcc15dfc31d0b96ca52961fa66d0e55af
Bhanu prakash Guddeti <100369843+Bhanuprakashgu@users.noreply.github.com>
2025-07-26 17:49:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -414,7 +414,7 @@ function App() {
               <span>Let's Collaborate</span>
               <div className="btn-ripple"></div>
             </button>
-            <a href="https://drive.google.com/file/d/15YXsW-x9gGoHIHm7hCryylGArnBSye4a/view" target="_blank" rel="noopener noreferrer" className="btn btn-secondary magnetic btn-large">
+            <a href="https://drive.google.com/file/d/1TYU8Az80wTmjJr6ASbVimMDGyKLpxAvI/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-secondary magnetic btn-large">
               <FaEnvelope className="btn-icon" />
               <span>View Resume</span>
               <div className="btn-ripple"></div>
